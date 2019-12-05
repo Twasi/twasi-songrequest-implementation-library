a2b408ac43fb34f27897ab7f3b010db7fae6465e
merlin <email@merlinw.de>
2019-12-05 19:43:04 +0100

    + Missing fields in SongDTO

--- src/models/Song.ts
+++ src/models/Song.ts
@@ -1,10 +1,19 @@
 import {PlaybackProvider} from "./PlaybackProvider";
 
 export interface Song {
+    requester: Requester;
+    timestamp: number;
     uri: string;
     provider: PlaybackProvider,
     name: string,
     artists: Array<string>,
     covers?: Array<string>,
     duration: number
+}
+
+export interface Requester {
+    displayName: string;
+    userName: string;
+    twitchId: string;
+    avatar: string;
 }
